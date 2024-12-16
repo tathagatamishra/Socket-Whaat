07ef27dd5c64e93a00869ed07efe0c4e08480bd6
tathagatamishra <tathagatamishra007@gmail.com>
2024-12-16 17:50:18 +0530

    anim

--- TCP-Room/frontend/components/Room.js
+++ TCP-Room/frontend/components/Room.js
@@ -13,27 +13,22 @@ export default function Room() {
     "And Password",
   ];
 
+  const arr = [1, 2, 3, 4, 5, 6, 7];
+
   useEffect(() => {
-    setMessages(preChat[0]);
+    setMessages([preChat[0]]);
   }, []);
 
   useEffect(() => {
-    if (currentIndex < preChat.length - 1) {
-      const timer = setTimeout(() => {
-        setMessages((prevMessages) => {
-          // Only add the next message if it's not already in the messages
-          if (!prevMessages.includes(preChat[currentIndex + 1])) {
-            return [...prevMessages, preChat[currentIndex + 1]];
-          }
-          return prevMessages;
-        });
-        setCurrentIndex((prev) => prev + 1);
-      }, 5000);
+    if (currentIndex >= 3) return;
 
-      // Clean up the timer to prevent memory leaks
-      return () => clearTimeout(timer);
-    }
-  }, [messages, currentIndex]);
+    const timer = setTimeout(() => {
+      setMessages([...messages, preChat[currentIndex + 1]]);
+      setCurrentIndex((prev) => prev + 1);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, [messages, preChat]);
 
   return (
     <div className="flex flex-col justify-between gap-[40px] items-center min-h-screen p-[40px]">
@@ -44,20 +39,44 @@ export default function Room() {
         TCP ROOM
       </h1>
 
-      <div className="flex flex-col gap-[20px] w-full h-full max-w-[600px] p-[20px]">
+      <div
+        className="flex flex-col justify-end gap-[20px] w-full h-[500px] max-w-[600px] p-[20px] border"
+        // style={{ boxShadow: "0px 80px 60px -50px red inset" }}
+      >
         {messages.map((message, index) => (
           <div
             key={index}
-            className="message relative block w-fit max-w-[80%] py-[14px] px-[20px] rounded-[20px] bg-[black] border-t-[1px] border-l-[1px] border-[#242424]"
+            className="message relative block w-fit max-w-[80%] py-[14px] px-[20px] rounded-[20px] bg-[black] border-t-[1px] border-l-[1px] border-[#242424] z-[9]"
           >
             <p>{message}</p>
 
             <div className="absolute block w-[20px] h-[20px] bg-[black] rounded-br-[4px] rotate-[45deg] left-[24px] bottom-[-9px]"></div>
           </div>
         ))}
+
+        <div
+          className="w-full flex flex-row gap-[10px] mt-[60px] z-[10]"
+          style={{ boxShadow: "0px -40px 50px 0px #171717", background: "#171717" }}
+        >
+          <input
+            className="w-full bg-[black] text-[white] rounded-[50px] px-[12px] py-[8px]"
+            type="text"
+          />
+          <button className="min-w-[40px] min-h-[40px] rounded-full p-[0px] flex flex-col items-center justify-center text-center text-[black] bg-[#fff] active:scale-[90%]">
+            &#11166;
+          </button>
+        </div>
       </div>
 
-      <div className="w-full">GitHub</div>
+      <div className="w-full">
+        <a
+          target="_blank"
+          className="text-[white]"
+          href="https://github.com/tathagatamishra/Socket-Whaat"
+        >
+          GitHub
+        </a>
+      </div>
     </div>
   );
 }
